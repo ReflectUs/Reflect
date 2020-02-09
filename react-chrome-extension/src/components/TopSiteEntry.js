2346570a43695e9476c8a577596cbd34f3e9dd5d
Varun Jindal <vjindal0112@gmail.com>
2020-02-08 19:26:11 -0500

    started on TopSites

---
+++ react-chrome-extension/src/components/TopSiteEntry.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+const Block = div.styled`
+  border-radius: 10px;
+
+
+`;
+
+export default class TopSiteEntry extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { site, time } = this.props
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
