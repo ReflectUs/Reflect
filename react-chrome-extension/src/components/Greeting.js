c22bdb1b28cf7c4b635baf769c9512d990a8513c
Varun Jindal <vjindal0112@gmail.com>
2020-03-05 10:38:19 -0500

    left aligned greeting

--- react-chrome-extension/src/components/Greeting.js
+++ react-chrome-extension/src/components/Greeting.js
@@ -4,17 +4,17 @@ import styled from "styled-components";
 const Hello = styled.h1`
   font-size: 64px;
   position: absolute;
-  top: 50%;
+  top: 30%;
   margin: 0;
   padding: 0;
-  left: 50%;
-  transform: translateX(-50%) translateY(-50%);
+  left: 20px;
+  /* transform: translateX(-50%) translateY(-50%); */
   color: #fff;
 `;
 export default class Header extends Component {
   render() {
     let name = localStorage.getItem("name")
 
-    return <Hello>Hello{name ? ', ' + name.split(" ")[0] + '!' : ""}</Hello>;
+    return <Hello>Good Morning{name ? ', ' + name.split(" ")[0] + '!' : ""}</Hello>;
   }
 }
